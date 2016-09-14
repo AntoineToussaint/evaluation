b0085b383464e960eb6aab20b630d7514074d720
Antoine Toussaint <caraphon@me.com>
2016-09-14 10:52:14 -0400

    Adding the test data and the requirements for pip to generate them.

--- test/unittest_autogen.cpp
+++ test/unittest_autogen.cpp
@@ -3,14 +3,18 @@
 #include "../src/evaluation.h"
 #include "../src/parser.h"
 
+
+using namespace boost::unit_test;
+
+
 BOOST_AUTO_TEST_CASE(Test_0)
 {
 // X=3
 // Y=X+1+2+z
 auto context = EvaluationParser::CreateFromFile("data/test_0.xml");
 context.setVariable("z", 0.5);
-BOOST_CHECK(3 == context.calc("X"));
-BOOST_CHECK(6.5 == context.calc("Y"));
+BOOST_TEST(6.5 == context.calc("Y"));
+BOOST_TEST(3 == context.calc("X"));
 }
 
 
@@ -20,7 +24,7 @@ BOOST_AUTO_TEST_CASE(Test_1)
 // Z=1*X
 auto context = EvaluationParser::CreateFromFile("data/test_1.xml");
 context.setVariable("y", 0.5);
-BOOST_CHECK(1.6487212707001282 == context.calc("X"));
-BOOST_CHECK(1.6487212707001282 == context.calc("Z"));
+BOOST_TEST(1.6487212707001282 == context.calc("X"));
+BOOST_TEST(1.6487212707001282 == context.calc("Z"));
 }
 
