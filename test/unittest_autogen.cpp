16570b2fabaa626029aca5272e2b517187996b40
Antoine Toussaint <caraphon@me.com>
2016-09-14 12:48:54 -0400

    Step by step

--- test/unittest_autogen.cpp
+++ test/unittest_autogen.cpp
@@ -12,9 +12,9 @@ BOOST_AUTO_TEST_CASE(Test_0)
 // X=3
 // Y=X+1+2+z
 auto context = EvaluationParser::CreateFromFile("data/test_0.xml");
-//context.setVariable("z", 0.5);
-//BOOST_CHECK(3 == context.calc("X"));
-//BOOST_CHECK(6.5 == context.calc("Y"));
+context.setVariable("z", 0.5);
+BOOST_CHECK(3 == context.calc("X"));
+BOOST_CHECK(6.5 == context.calc("Y"));
 }
 
 /*
